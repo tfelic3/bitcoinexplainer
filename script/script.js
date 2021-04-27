2e0b2d87a99f2583711ead1ebdc4ac74f5833ad3
tfelic3 <felicien.tesalon@gmail.com>
2021-04-27 08:03:09 -0400

    New changes

--- script/script.js
+++ script/script.js
@@ -19,13 +19,7 @@ const data = [{
   caption: 'Credit: CNN',
   answer: "Bitcoin’s value changes all the time because its supply is limited, and demand for it changes constantly, sorta like stocks. According to Investopedia, one Bitcoin was worth 2/25 or less than one-tenth of a penny when it started trading in 2009. It jumped to a whopping 8 cents a year later. By April 2021, one Bitcoin rose above $64,000.",
 },
-{
-  cardHeadline: "Is there a central bank backing Bitcoin?",
-  content: "video/bitcointheft.mp4",
-  subhead: "Nope. You’re kinda on your own",
-  caption: 'Credit: KUTV/Brandon Larsen/CNN',
-  answer: "There’s no central bank backing Bitcoin. It’s not backed by a physical commodity like gold. And nothing protects your “wallet,” the way the Federal Deposit Insurance Corporation (FDIC) insures your deposit in a bank. Servers where “wallets” are in the cloud have been hacked. And you can accidentally delete your wallet on your computer.",
-},
+
 {
   cardHeadline: "What can I buy with Bitcoin?",
   content: "img/bitcoinpurchase.jpg",
@@ -41,7 +35,7 @@ const data = [{
   answer: "Well, nobody knows.  A person or group using the pseudonym Satoshi Nakamoto created it in 2009.",
 },
 {
-  cardHeadline: "What is “mining bitcoin”?",
+  cardHeadline: "How does mining Bitcoin affect Earth?",
   subhead: "Bitcoin uses loads of electricity",
   content: "img/bitcoinmining.jpg",
   caption: 'Credit: CNN',
@@ -76,87 +70,87 @@ nextQuestionButton.textContent = data[divIndex + 1].cardHeadline;
 previousButton.textContent = data[divIndex].cardHeadline;
 
 card.addEventListener("click", function() {
-card.classList.add("is-flipped");
-directions.style.display = 'none';
+    card.classList.add("is-flipped");
+    directions.style.display = 'none';
 });
 
 previousButton.addEventListener('click', function() {
-divIndex = divIndex - 1;
-if (divIndex == 0) {
-  previousButton.style.display = 'none';
-}
+    divIndex = divIndex - 1;
+    if (divIndex == 0) {
+        previousButton.style.display = 'none';
+    }
 
-if (data[divIndex].content.includes('mp4')) {
-  videoDiv.src = data[divIndex].content;
-  videoDiv.style.display = 'block';
-  imageCaption.style.display = 'none';
+    if (data[divIndex].content.includes('mp4')) {
+        videoDiv.src = data[divIndex].content;
+        videoDiv.style.display = 'block';
+        imageCaption.style.display = 'none';
 
-} else {
-  videoDiv.style.display = 'none';
-  imageCaption.style.display = 'block';
-  videoDiv.pause();
+    } else {
+        videoDiv.style.display = 'none';
+        imageCaption.style.display = 'block';
+        videoDiv.pause();
 
-}
+    }
 
-//console.log('back divIndex ', divIndex);
-//console.log(data[divIndex])
-cardSubHead.textContent = data[divIndex].subhead;
-imageDiv.src = data[divIndex].content;
-description.textContent = data[divIndex].answer;
+    //console.log('back divIndex ', divIndex);
+    //console.log(data[divIndex])
+    cardSubHead.textContent = data[divIndex].subhead;
+    imageDiv.src = data[divIndex].content;
+    description.textContent = data[divIndex].answer;
 
-if (divIndex != 0) {
-previousButton.textContent = data[divIndex - 1].cardHeadline;
-}
-nextQuestionButton.textContent = data[divIndex + 1].cardHeadline;
+    if (divIndex != 0) {
+      previousButton.textContent = data[divIndex - 1].cardHeadline;
+    }
+    nextQuestionButton.textContent = data[divIndex + 1].cardHeadline;
 
 })
 
 nextQuestionButton.addEventListener('click', function() {
 
-divIndex += 1;
-//previousIndex = buttonIndex - 1;
-//console.log('previous index ', previousIndex);
-//console.log('button index ', buttonIndex - 1)
-//console.log('next divIndex ', divIndex)
+    divIndex += 1;
+    //previousIndex = buttonIndex - 1;
+    //console.log('previous index ', previousIndex);
+    //console.log('button index ', buttonIndex - 1)
+    //console.log('next divIndex ', divIndex)
 
-if (divIndex === 7) {
-location.reload();
-divIndex = 0;
+    if (divIndex === 7) {
+      location.reload();
+      divIndex = 0;
 
-}
+    }
 
-if (divIndex === 6) {
-nextQuestionButton.textContent = 'Reset';
-}
-previousButton.style.display = 'block';
+    if (divIndex === 6) {
+      nextQuestionButton.textContent = 'Reset';
+    }
+    previousButton.style.display = 'block';
 
-cardBack.style.animation = 'fadein 2s'
+    cardBack.style.animation = 'fadein 2s'
 
-cardSubHead.textContent = data[divIndex].subhead;
-imageDiv.src = data[divIndex].content;
-imageCaption.textContent = data[divIndex].caption;
-description.textContent = data[divIndex].answer;
-previousButton.textContent = data[divIndex - 1].cardHeadline;
+    cardSubHead.textContent = data[divIndex].subhead;
+    imageDiv.src = data[divIndex].content;
+    imageCaption.textContent = data[divIndex].caption;
+    description.textContent = data[divIndex].answer;
+    previousButton.textContent = data[divIndex - 1].cardHeadline;
 
-if (divIndex !== 6) {
-nextQuestionButton.textContent = data[divIndex + 1].cardHeadline;      
-}
+    if (divIndex !== 6) {
+      nextQuestionButton.textContent = data[divIndex + 1].cardHeadline;      
+    }
 
-if (data[divIndex].content.includes('mp4')) {
-  videoDiv.src = data[divIndex].content;
-  videoDiv.style.display = 'block';
-  imageCaption.style.display = 'none';
-} else {
-  videoDiv.style.display = 'none';
-  imageCaption.style.display = 'block';
-  videoDiv.pause();
-}
+    if (data[divIndex].content.includes('mp4')) {
+        videoDiv.src = data[divIndex].content;
+        videoDiv.style.display = 'block';
+        imageCaption.style.display = 'none';
+    } else {
+        videoDiv.style.display = 'none';
+        imageCaption.style.display = 'block';
+        videoDiv.pause();
+    }
 
-setTimeout(
-  function() {
-      cardBack.removeAttribute('style');
-  }, 3000
-)
+    setTimeout(
+        function() {
+            cardBack.removeAttribute('style');
+        }, 3000
+    )
 
 
 });
